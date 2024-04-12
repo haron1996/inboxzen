1b35816763335d38d684447c3f978efed6eed5b3
Kwandapchumba <kwandapchumba@gmail.com>
2024-04-12 05:20:48 +0300

    menu change

--- frontend/src/routes/dashboard/Menu.svelte
+++ frontend/src/routes/dashboard/Menu.svelte
@@ -159,7 +159,15 @@
 			}
 
 			&:hover {
-				background-color: $gray;
+				background-color: #4e54c8;
+				background-image: linear-gradient(
+					to right top,
+					#4e54c8,
+					#8f94fb,
+					#b4b8fd,
+					#cad3ff,
+					#e5f0ff
+				);
 			}
 		}
 	}
